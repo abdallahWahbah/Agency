5f4b7af8ec0c2df6dc225af68b5affaa5613ce3b
abdowahbah <abdowahbah123@yahoo.com>
2021-10-02 00:02:58 +0200

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,59 @@
+// navidation on mobile mode
+document.querySelector(".navigation").addEventListener("click", e =>
+{
+    const element = e.target.closest(".navigation__button");
+    if(!element) return;
+    document.querySelector(".navigation__list").classList.toggle("active");
+});
+
+// Slider
+const slideWork = ()=>
+{
+    const slides = document.querySelectorAll(".slide");
+    const btnLeft = document.querySelector(".slider__btn--left");
+    const btnRight = document.querySelector(".slider__btn--right");
+
+    let curSlide = 0;
+    let maxSlides = slides.length;
+
+    const goToSlide = (slide) =>
+    {
+        slides.forEach((sl, i) =>
+        {
+            sl.style.transform = `translateX(${100 * ( i - slide )}%)`;
+        });
+    };
+    const nextSlide = () =>
+    {
+        if(curSlide === maxSlides - 1) curSlide = 0;
+        else curSlide++;
+        goToSlide(curSlide);
+        // activateDot(curSlide);
+    };
+    const prevSlide = () =>
+    {
+        if(curSlide === 0) curSlide = maxSlides - 1;
+        else curSlide--;
+        goToSlide(curSlide);
+        // activateDot(curSlide);
+    };
+    const init =() =>
+    {
+        goToSlide(0);
+        // createDots();
+        // activateDot(0);
+    }
+    init();
+
+    // Event handlers
+    btnRight.addEventListener("click",nextSlide);
+    btnLeft.addEventListener("click",prevSlide);
+
+    // Keyboard right and left arrow
+    document.addEventListener("keydown", (e)=>
+    {
+        if(e.key === "ArrowLeft") prevSlide();
+        else if (e.key === "ArrowRight") nextSlide();
+    });
+}
+slideWork();
